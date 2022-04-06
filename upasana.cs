01ffc1c555f506434cdd2384bf32e531749af8c7
UpasanaAulakh <101379151+UpasanaAulakh@users.noreply.github.com>
2022-04-06 17:17:58 +0530

    Add files via upload

---
+++ upasana.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            arr[0] = 10;
+            arr[1] = 20;
+            arr[2] = 30;
+            arr[3] = 40;
+            arr[4] = 50;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
+
+
+
+
+            
+        }
+    }
+}
